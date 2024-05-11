e4593cc76cf22e8c62ae7e0f8c00e35abecc25dd
omerbenda <34948058+omerbenda@users.noreply.github.com>
2024-05-11 05:09:05 +0300

    fix(SolarFurnaceBlock.java): Fix solar furnace not smelting items

--- src/main/java/com/sunsembrace/block/custom/SolarFurnaceBlock.java
+++ src/main/java/com/sunsembrace/block/custom/SolarFurnaceBlock.java
@@ -1,20 +1,28 @@
 package com.sunsembrace.block.custom;
 
 import com.mojang.serialization.MapCodec;
+import com.sunsembrace.blockentity.ModBlockEntities;
 import com.sunsembrace.blockentity.custom.SolarFurnaceBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.AbstractFurnaceBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
-public class SolarFurnaceBlock extends AbstractSolarFurnaceBlock {
+public class SolarFurnaceBlock extends AbstractFurnaceBlock {
   public static final MapCodec<SolarFurnaceBlock> CODEC = simpleCodec(SolarFurnaceBlock::new);
 
   public SolarFurnaceBlock(Properties properties) {
     super(properties);
+  }
+
+  @Override
+  protected MapCodec<? extends SolarFurnaceBlock> codec() {
+    return CODEC;
   }
 
   @Override
@@ -27,8 +35,10 @@ public class SolarFurnaceBlock extends AbstractSolarFurnaceBlock {
   }
 
   @Override
-  protected MapCodec<? extends BaseEntityBlock> codec() {
-    return CODEC;
+  public <T extends BlockEntity> BlockEntityTicker<T> getTicker(
+      Level pLevel, BlockState pState, BlockEntityType<T> pBlockEntityType) {
+    return createFurnaceTicker(
+        pLevel, pBlockEntityType, ModBlockEntities.SOLAR_FURNACE_BLOCK_ENTITY.get());
   }
 
   @Nullable
